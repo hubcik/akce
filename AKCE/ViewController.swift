09e4e21b391ecc78ebf84667f39c57a4d2bfe507
Hubert Gostomski <hubert.gostomski@gmail.com>
2018-12-27 16:50:16 +0100

    Initial Commit

---
+++ AKCE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AKCE
+//
+//  Created by Hubert Gostomski on 27/12/2018.
+//  Copyright © 2018 Hubert Gostomski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
